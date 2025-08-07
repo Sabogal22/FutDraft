1268c772067a37d4fede16a0e5a036a1fc2c14cc
Sabogal22 <nsabogalgaitan@gmail.com>
2025-08-06 21:46:04 -0500

    Agregar tipo Manager y datos de ejemplo para los entrenadores en manager.ts

---
+++ src/data/manager.ts
@@ -0,0 +1,58 @@
+type Manager = {
+  id: number;
+  nombre: string;
+  imagen: string;
+  nacionalidad: string;
+  backup_image: string;
+};
+
+export const allManager: Manager[] = [
+  {
+    id: 1,
+    nombre: "Pep Guardiola",
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Pep_Guardiola_2019.jpg/220px-Pep_Guardiola_2019.jpg",
+    nacionalidad: "España",
+    backup_image: "/imgs/cards/manager-card.png",
+  },
+  {
+    id: 2,
+    nombre: "Jürgen Klopp",
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/J%C3%BCrgen_Klopp_2019.jpg/220px-J%C3%BCrgen_Klopp_2019.jpg",
+    nacionalidad: "Alemania",
+    backup_image: "/imgs/cards/manager-card.png",
+  },
+  {
+    id: 3,
+    nombre: "Carlo Ancelotti",
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Carlo_Ancelotti_2015.jpg/220px-Carlo_Ancelotti_2015.jpg",
+    nacionalidad: "Italia",
+    backup_image: "/imgs/cards/manager-card.png",
+  },
+  {
+    id: 4,
+    nombre: "ejemplo 1",
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Pep_Guardiola_2019.jpg/220px-Pep_Guardiola_2019.jpg",
+    nacionalidad: "España",
+    backup_image: "/imgs/cards/manager-card.png",
+  },
+  {
+    id: 5,
+    nombre: "ejemplo 2",
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/J%C3%BCrgen_Klopp_2019.jpg/220px-J%C3%BCrgen_Klopp_2019.jpg",
+    nacionalidad: "Alemania",
+    backup_image: "/imgs/cards/manager-card.png",
+  },
+  {
+    id: 6,
+    nombre: "ejemplo 3",
+    imagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Carlo_Ancelotti_2015.jpg/220px-Carlo_Ancelotti_2015.jpg",
+    nacionalidad: "Italia",
+    backup_image: "/imgs/cards/manager-card.png",
+  },
+];
